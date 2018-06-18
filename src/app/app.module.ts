988c592791477acd16530f38000d1556a08ce826
Guest User <Guest@epicodus-9.local>
2018-06-18 10:57:31 -0700

    create a welcome component, app.routing.ts file, update gitignore to ignore api-keys file, update index and app.component.html file, update dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing } from './app.routing';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
