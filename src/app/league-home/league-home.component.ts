e4162ad13e0e7b0a8b2fa5af9cfb9ce60c169d18
andrew lupton <aflupton@gmail.com>
2018-06-19 16:07:15 -0700

    add dependencies

--- src/app/league-home/league-home.component.ts
+++ src/app/league-home/league-home.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { LeagueService } from '../league.service';
-import { League } from '../models/league.model';
+// import { LeagueService } from '../league.service';
+// import { League } from '../models/league.model';
+import { Team } from '../models/team.model';
+import { TeamService } from '../team.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-league-home',
   templateUrl: './league-home.component.html',
   styleUrls: ['./league-home.component.css'],
-  providers: [LeagueService]
+  providers: [TeamService]
 })
 export class LeagueHomeComponent implements OnInit {
-  leagueId: string;
-  leagueToDisplay;
+  teamId: string;
+  teamToDisplay;
+  teams: FirebaseListObservable<any[]>;
 
-  constructor(private route: ActivatedRoute, private location: Location, private leagueService: LeagueService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private teamService: TeamService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
-      this.leagueId = urlParameters['id'];
+      this.teamId = urlParameters['id'];
     });
-    this.leagueToDisplay = this.leagueService.getLeagueById(this.leagueId);
+    this.teamToDisplay = this.teamService.getTeamById(this.teamId);
   }
 
 }
