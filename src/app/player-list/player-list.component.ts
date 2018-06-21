6a6bca4bbef4b48f0e7e10145ea1e741b2fb07f8
Guest User <Guest@epicodus-9.local>
2018-06-21 09:23:27 -0700

    add getplayers to teamservice. add data to player-list component.

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../team.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
-  styleUrls: ['./player-list.component.css']
+  styleUrls: ['./player-list.component.css'],
+  providers: [TeamService]
 })
 export class PlayerListComponent implements OnInit {
 
-  constructor() { }
+  players: FirebaseListObservable<any[]>;
+  constructor(private router: Router, private teamService: TeamService) { }
 
   ngOnInit() {
+    this.players = this.teamService.getPlayers();
   }
 
 }
